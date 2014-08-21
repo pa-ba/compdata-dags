089355a9794d4e26dfd17ac423873581b6b04d53
Patrick Bahr <patrick.bahr@gmail.com>
2014-08-21 08:20:37 +0200

    implement bisimilarity check

--- src/Data/Comp/Dag.hs
+++ src/Data/Comp/Dag.hs
@@ -10,6 +10,7 @@ module Data.Comp.Dag
     , termTree
     , reifyDag
     , unravel
+    , bisim
     ) where
 
 
@@ -21,10 +22,13 @@ import Data.Comp.Term
 import qualified Data.HashMap.Lazy as HashMap
 import qualified Data.IntMap as IntMap
 import Data.IORef
+import Data.Foldable (Foldable)
 import Data.Traversable (Traversable)
 import qualified Data.Traversable as Traversable
 import Data.Typeable
 import System.Mem.StableName
+import Data.Comp.Equality
+
 
 -- | Turn a term into a graph without sharing.
 termTree :: Functor f => Term f -> Dag f
@@ -89,3 +93,24 @@ unravel Dag {edges, root} = Term $ build <$> root
     where build :: Context f Node -> Term f
           build (Term t) = Term $ build <$> t
           build (Hole n) = Term $ build <$> edges IntMap.! n
+
+-- | Checks whether two dags are bisimilar. In particular, we have
+-- the following equality
+-- 
+-- @
+-- bisim g1 g2 = (unravel g1 == unravel g2)
+-- @
+-- 
+-- That is, two dags are bisimilar iff they have the same unravelling.
+
+bisim :: forall f . (EqF f, Functor f, Foldable f)  => Dag f -> Dag f -> Bool
+bisim Dag {root=r1,edges=e1}  Dag {root=r2,edges=e2} = runF r1 r2
+    where run :: (Context f Node, Context f Node) -> Bool
+          run (t1, t2) = runF (step e1 t1) (step e2 t2)
+          step :: Edges f -> Context f Node -> f (Context f Node)
+          step e (Hole n) = e IntMap.! n
+          step _ (Term t) = t
+          runF :: f (Context f Node) -> f (Context f Node) -> Bool
+          runF f1 f2 = case eqMod f1 f2 of
+                         Nothing -> False
+                         Just l -> all run l
--- src/Data/Comp/Dag/Internal.hs
+++ src/Data/Comp/Dag/Internal.hs
@@ -6,6 +6,9 @@ import Data.Comp.Term
 import Data.IntMap (IntMap)
 
 type Node = Int
+type Edges f = IntMap (f (Context f Node))
+
+
 data Dag f = Dag { root      :: f (Context f Node)
-                 , edges     :: IntMap (f (Context f Node))
+                 , edges     :: Edges f
                  , nodeCount :: Int }
