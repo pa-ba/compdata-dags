68b08deed6f78c9e35a3f244500d9b8b5521a4e1
Patrick Bahr <patrick.bahr@gmail.com>
2015-01-14 11:39:25 +0530

    improve implementation of repmin on DAGs
    It produces DAGs with only a single leaf node.

--- examples/Examples/RepminPAG.hs
+++ examples/Examples/RepminPAG.hs
@@ -4,6 +4,14 @@
 {-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DeriveTraversable #-}
 
+-- This is an implementation of repmin as a PAG. The use of a PAG
+-- allows us to implement repmin such that the result of repmin is a
+-- DAG with only one leaf node, which is shared throughout the
+-- DAG. This is achieved as follows instead of only collecting the
+-- minimum synthesised attribute and then turning it into an inherited
+-- attribute, which propagates the minimum to the leaves of the graph,
+-- we construct a single leaf node with the minimum labelling and
+-- propagate it downwards as an inherited attribute.
 
 module Examples.RepminPAG where
 
@@ -18,11 +26,8 @@ import Data.Foldable
 
 
 newtype MinS a = MinS {unMinS :: Int} deriving (Eq,Ord,Functor, Foldable, Traversable)
-newtype MinI a = MinI Int deriving (Functor, Foldable, Traversable)
+newtype MinI a = MinI a deriving (Functor, Foldable, Traversable)
 
-
-globMin :: (?above :: atts a, MinI :< atts) => Int
-globMin = let MinI i = above in i
 
 minS ::  Syn IntTreeF atts MinS f
 minS (Leaf i)    =  MinS i
@@ -32,18 +37,16 @@ minI :: Inh IntTreeF atts MinI f
 minI _ = empty
 
 rep ::  (MinI :< atts) => Syn IntTreeF atts I IntTreeF
-rep (Leaf _)    =  I $ iLeaf globMin
+rep (Leaf _)    =  let MinI n = above in I (Hole n)
 rep (Node a b)  =  I $ iNode (Hole $ unI $ below a) (Hole $ unI $ below b)
-
-
 
 
 repminG :: Dag IntTreeF -> Dag IntTreeF
 repminG = unI . fsnd . Dag.runPAG const (minS |*| rep) minI  init
-  where init (MinS i :*: _) = MinI i
+  where init (MinS i :*: _) = MinI (iLeaf i)
 
 
 repmin :: Term IntTreeF -> Term IntTreeF
 repmin = unI . fsnd . runPAG (minS |*| rep) minI  init
-  where init (MinS i :*: _) = MinI i
+  where init (MinS i :*: _) = MinI (iLeaf i)
 
